a5004ec6dd8186bfab6eff3d95895d3c61965306
SametFurkanUnlukaya <sametfurkanunlukaya@hotmail.com>
2025-04-28 13:44:15 +0300

    Ön Tasarım

--- index.php
+++ index.php
@@ -1,17 +1,38 @@
 <?php
-// PHP kodu burada başlar
-echo "Merhaba kullanıcı!";
+// İleride kullanıcı giriş yaptıysa dashboard'a yönlendirme kodu buraya eklenecek
 ?>
 
 <!DOCTYPE html>
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <title>Sinema Projesi</title>
+    <title>scene'SFrame | SF Sinemaları</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
-<h2>Sinema Projesine Hoş Geldinizzzzzzzz</h2>
+    <header>
+        <h1 class="logo">scene'SFrame</h1>
+    </header>
+
+    <main>
+        <section class="welcome-box">
+            <h2>SF Sinemalarına Hoşgeldiniz!</h2>
+            <div class="button-group">
+                <a href="pages/register.php" class="btn">Kayıt Ol</a>
+                <a href="pages/login.php" class="btn">Giriş Yap</a>
+            </div>
+        </section>
+
+        <section class="movies-box">
+            <h2>En Yeni Filmleri Görmeye Hazır mısın?</h2>
+            <p>Biletini al, sahnelerin en güzel çerçevesine ulaş!</p>
+        </section>
+    </main>
+
+    <footer>
+        <p>&copy; 2025 SF Sinemaları - scene'SFrame</p>
+    </footer>
 
 </body>
 </html>
