17010d37b7f03dd1a0cc01d9f8d10037f120e4e1
SametFurkanUnlukaya <sametfurkanunlukaya@hotmail.com>
2025-04-28 12:23:16 +0300

    İlk proje başlangıcı - scneSFrame kuruldu

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// PHP kodu burada başlar
+echo "Merhaba kullanıcı!";
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Sinema Projesi</title>
+</head>
+<body>
+
+<h2>Sinema Projesine Hoş Geldinizzzzzzzz</h2>
+
+</body>
+</html>
